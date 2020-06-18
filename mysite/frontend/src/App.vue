755ad2c76675765c62c622f97f53ee5bd57ea733
Cris <criston2011@gmail.com>
2020-06-18 14:02:44 +0530

    Vue3 Django template working..

---
+++ mysite/frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>This is a Vue 3 component!</h2>
+    <button @click="increase">Clicked {{ count }} times.</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  setup() {
+    const count = ref(0);
+    const increase = () => {
+      count.value++;
+    };
+
+    return {
+      count,
+      increase
+    };
+  }
+});
+</script>
+
+<style scoped>
+</style>
